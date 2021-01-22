1c9ddfd346aed424ff41bcf178243008cc02b401
Diego <alvarodiego@gmail.com>
2021-01-22 19:22:26 -0300

    Classe Instrutor criada

---
+++ src/Entities/Instrutor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace src.Entities
+{
+    class Instrutor : Usuario
+    {
+        public List<Curso> CursosMinistrados { get; private set; }
+
+        public Instrutor(int id, string nome) : base(id, nome)
+        {
+            CursosMinistrados = null;
+        }
+
+        public void AddCursoMinistrado(Curso curso)
+        {
+            CursosMinistrados.Add(curso);
+        }
+    }
+}
