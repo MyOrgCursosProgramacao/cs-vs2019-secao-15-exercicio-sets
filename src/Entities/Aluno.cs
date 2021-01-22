3b0fa51b66ebd3d769fcb9dd98d8f0d67f3ba764
Diego <alvarodiego@gmail.com>
2021-01-22 19:22:16 -0300

    Classe Aluno criada

---
+++ src/Entities/Aluno.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace src.Entities
+{
+    class Aluno : Usuario
+    {
+        public List<Curso> Cursos { get; private set; }
+
+        public Aluno(int id, string nome) : base(id, nome)
+        {
+        }
+    }
+}
